b9420861bc4a574be21b9f2a8de711c863cde490
ytrbjbytbtr <52233543+ytrbjbytbtr@users.noreply.github.com>
2023-06-13 08:47:01 +0300

    Add files via upload

---
+++ ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,70 @@
+﻿using MySqlX.XDevAPI.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp15
+{
+    abstract class MealBase
+    {
+        public void Make()
+        {
+            Prepare();
+            Cook();
+            FinalSteps();
+        }
+        protected abstract void Prepare();
+        protected abstract void Cook();
+        protected abstract void FinalSteps();
+    }
+
+    class PotatoMeal : MealBase
+    {
+        protected override void Cook()
+        {
+            Console.WriteLine("Ставим почищенную картошку на огонь");
+            Console.WriteLine("Варим около 30 минут");
+            Console.WriteLine("Сливаем остатки воды, разминаем варенный картофель в пюре");
+        }
+
+        protected override void FinalSteps()
+        {
+            Console.WriteLine("Посыпаем пюре специями и зеленью");
+            Console.WriteLine("Картофельное пюре готово");
+        }
+
+        protected override void Prepare()
+        {
+            Console.WriteLine("Чистим и моем картошку");
+        }
+    }
+
+    class SaladMeal : MealBase
+    {
+        protected override void Cook()
+        {
+            Console.WriteLine("Нарезаем помидоры и огурцы");
+            Console.WriteLine("Посыпаем зеленью, солью и специями");
+        }
+
+        protected override void FinalSteps()
+        {
+            Console.WriteLine("Поливаем подсолнечным маслом");
+            Console.WriteLine("Салат готов");
+        }
+
+        protected override void Prepare()
+        {
+            Console.WriteLine("Моем помидоры и огурцы");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
